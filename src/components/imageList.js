4b4ed5ddf5f80b18682632995802ac85b3c2ad59
Haizhou Chen <higocn@163.com>
2019-09-21 17:54:32 +0800

    update preload images

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -7,9 +7,37 @@ import bg6 from '../assets/images/bg/bg6.png'
 import bg7 from '../assets/images/bg/bg7.png'
 import bag from '../assets/images/bag.png'
 import play from '../assets/images/play.png'
+import card from '../assets/images/card.png'
 import stop from '../assets/images/stop.png'
 import parrot from '../assets/images/parrot.png'
 import parrot2 from '../assets/images/parrot2.png'
+import p1 from '../assets/images/question/p1.png'
+import p2 from '../assets/images/question/p2.png'
+import p3 from '../assets/images/question/p3.png'
+import p4 from '../assets/images/question/p4.png'
+import p5 from '../assets/images/question/p5.png'
+import p1q2 from '../assets/images/question/p1_2.png'
+import p2q2 from '../assets/images/question/p2_2.png'
+import p3q2 from '../assets/images/question/p3_2.png'
+import p4q2 from '../assets/images/question/p4_2.png'
+import p5q2 from '../assets/images/question/p5_2.png'
+import cage from '../assets/images/img/cage.png'
+import chainsaw from '../assets/images/img/chainsaw.png'
+import falcon from '../assets/images/img/falcon.png'
+import food from '../assets/images/img/food.png'
+import foot from '../assets/images/img/foot.png'
+import leopard from '../assets/images/img/leopard.png'
+import leopard2 from '../assets/images/img/leopard2.png'
+import panda from '../assets/images/img/panda.png'
+import panda2 from '../assets/images/img/panda2.png'
+import panda3 from '../assets/images/img/panda3.png'
+import mouse from '../assets/images/img/mouse.png'
+import plume from '../assets/images/img/plume.png'
+import monkey from '../assets/images/img/monkey.png'
+import logo1 from '../assets/images/logo/bhgjjjh.png'
+import logo2 from '../assets/images/logo/dxmjd.png'
+import logo3 from '../assets/images/logo/dzrxh.png'
+import logo4 from '../assets/images/logo/sjdwxh.png'
 
 export default [
   bg1,
@@ -21,7 +49,35 @@ export default [
   bg7,
   bag,
   play,
+  card,
   stop,
   parrot,
-  parrot2
+  parrot2,
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p1q2,
+  p2q2,
+  p3q2,
+  p4q2,
+  p5q2,
+  cage,
+  chainsaw,
+  falcon,
+  food,
+  foot,
+  plume,
+  leopard,
+  leopard2,
+  panda,
+  panda2,
+  panda3,
+  mouse,
+  monkey,
+  logo1,
+  logo2,
+  logo3,
+  logo4
 ]
